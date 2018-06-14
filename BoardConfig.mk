20f38a107fe8afdf9a16ce4b901931086004f0fe
MkfsSion <2875935638@qq.com>
2018-06-14 19:18:29 +0800

    wt88047: BoardConfig: Cleanup for TWRP build.
    Signed-off-by: MkfsSion <2875935638@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,19 +16,12 @@
 
 DEVICE_PATH := device/wingtech/wt88047
 
+# Board
 TARGET_BOARD_PLATFORM := msm8916
-TARGET_BOOTLOADER_BOARD_NAME := msm8916
+BOOTLOADER_PLATFORM := msm8916
+TARGET_BOARD_SUFFIX := _64
 
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_APPEND_DTB := false
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := wt88047_64,WT88047_64,wt88047,WT88047,wt86047,WT86047,HM2014811,HM2014812,HM2014813,HM2014814,HM2014815,HM2014816,HM2014817,HM2014818,HM2014819,HM2014820,HM2014821
-
-USE_CLANG_PLATFORM_BUILD := true
-
-
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+# Arch
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -41,13 +34,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := false
-
-BOOTLOADER_PLATFORM := msm8916 # use msm8952 LK configuration
-
-MALLOC_SVELTE := true
-
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
@@ -59,169 +45,54 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 5939100672
 BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
-# partitions For Treble
+# Treble
 BOARD_VENDORIMAGE_PARTITION_SIZE := 400556032
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USES_VENDORIMAGE := true
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
 
-
-# Added to indicate that protobuf-c is supported in this build
-PROTOBUF_SUPPORTED := true
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
-  
-
-TARGET_USES_ION := true
-TARGET_USES_NEW_ION_API :=true
-TARGET_USES_QCOM_BSP := true
-TARGET_NO_RPC := true
-
-# Enable CSVT
-TARGET_USES_CSVT := true
-
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
-BOARD_KERNEL_SEPARATED_DT := true
-
-
+# Kernel
+BOARD_KERNEL_CMDLINE += \
+    sched_enable_hmp=1 \
+    phy-msm-usb.floated_charger_enable=1 \
+    androidboot.selinux=permissive
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
-BOARD_MKBOOTIMG_ARGS     := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
-BOARD_DTBTOOL_ARGS       := -2
-
+BOARD_MKBOOTIMG_ARGS :=  --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
 
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_USES_UNCOMPRESSED_KERNEL := false
-
-# Adreno
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-TARGET_CONTINUOUS_SPLASH_ENABLED := false
-TARGET_USES_C2D_COMPOSITION := true
-
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-
-# Add NON-HLOS files for ota upgrade
-ADD_RADIO_FILES := true
-
+# Init
 TARGET_INIT_VENDOR_LIB := libinit_wt88047
 TARGET_RECOVERY_DEVICE_MODULES := libinit_wt88047
 
-# init
-TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_PATH)/init/init_wt88047.cpp
-TARGET_UNIFIED_DEVICE := true
-
-#add suffix variable to uniquely identify the board
-TARGET_BOARD_SUFFIX := _64
-
-# Audio
-AUDIO_FEATURE_ENABLED_PM_SUPPORT := false
-AUDIO_FEATURE_ENABLED_SSR := false
-AUDIO_FEATURE_ENABLED_EXTN_RESAMPLER := false
-BOARD_SUPPORTS_SOUND_TRIGGER := false
-
-#Camera
-BOARD_CAMERA_SENSORS := \
-    ov2680_5987fhq \
-    ov8865_q8v18a \
-    ov2680_skuhf
-USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-TARGET_TS_MAKEUP := true
-
-# For the Legacy blobs
-TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-
-#GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
-
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
-#Enable HW based full disk encryption
+# Crypto
 TARGET_HW_DISK_ENCRYPTION := false
+TARGET_HW_KEYMASTER_V03 := true
+TW_INCLUDE_CRYPTO := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
-#Enable SW based full disk encryption
-#TARGET_SWV8_DISK_ENCRYPTION := true
-
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-
-MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
-
-# Enable sensor multi HAL
-#USE_SENSOR_MULTI_HAL := true
-
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
-FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
-
-# TWRP
-RECOVERY_VARIANT := twrp
+# TWRP Misc
 DEVICE_RESOLUTION := 720x1280
-TW_TARGET_USES_QCOM_BSP := false
 TW_NEW_ION_HEAP := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_INCLUDE_CRYPTO := true
+TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_DEFAULT_BRIGHTNESS := 120
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_NTFS_3G := true
+TW_IGNORE_MISC_WIPE_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
-# FM
-TARGET_QCOM_NO_FM_FIRMWARE := true
-AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
-BOARD_HAVE_QCOM_FM := true
-
-# Control flag between KM versions
-TARGET_HW_KEYMASTER_V03 := true
-
-#Enable peripheral manager
-TARGET_PER_MGR_ENABLED := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
-
-# Wifi
-BOARD_HAS_QCOM_WLAN := true
-BOARD_HAS_QCOM_WLAN_SDK := true
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
-BOARD_WLAN_DEVICE := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-TARGET_USES_QCOM_WCNSS_QMI := true
-TARGET_USES_WCNSS_CTRL := true
-WIFI_DRIVER_FW_PATH_AP := "ap"
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-
-WITH_DEXPREOPT := false
-ifneq ($(TARGET_BUILD_VARIANT),user)
-  # Retain classes.dex in APK's for non-user builds
-  DEX_PREOPT_DEFAULT := nostripping
-endif
-
-#enabling IMS for this target
-TARGET_USES_IMS := true
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
